b927855686283845b0ca30e8055ce4719d14a0c4
Lester <lester@ssagroup.com>
2024-08-28 12:11:15 +0800

    Fixed font-size and stay tuned page sizes

--- wp-content/themes/uaessagroup-theme/views/pages/partials-about/hero.php
+++ wp-content/themes/uaessagroup-theme/views/pages/partials-about/hero.php
@@ -39,7 +39,7 @@ $accordions = [
                             <div id="accordion-title" class="group flex justify-between px-4 py-3 items-center transition ease duration-500 cursor-pointer">
                                 <div class="transition ease duration-500">
                                     <h2 class="text-[18px] font-[700] leading-[30px]"><?= $accordion['title'] ?></h2>
-                                    <p class="text-xs text-gray-400 italic"><?= $accordion['sub'] ?></p>
+                                    <p class="text-sm text-gray-400 italic"><?= $accordion['sub'] ?></p>
                                 </div>
                                 <div id="accordion-carret" class="ease duration-500 group-focus:text-white <?= $key === 0 ? 'rotate-180' : '' ?> ">
                                     <i class="fas fa-chevron-down"></i>
